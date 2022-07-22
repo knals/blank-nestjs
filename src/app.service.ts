6641c4ef4ccdcdb0a27fd925bb2403cb6bc6e21e
knals <knals.developer@gmail.com>
2022-07-22 16:37:15 +0200

    testing mail sevice

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,71 @@
-import { Get, Injectable } from '@nestjs/common';
-import { Resource, Roles } from 'nest-keycloak-connect';
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
 
-@Resource(AppService.name)
 @Injectable()
 export class AppService {
+  constructor(private readonly mailerService: MailerService) { }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  public example(): void {
+    this
+      .mailerService
+      .sendMail({
+        to: 'knals.developer@gmail.com', // List of receivers email address
+        from: 'eduardo.canals.escriva@gmail.com', // Senders email address
+        subject: 'Testing Nest MailerModule ✔', // Subject line
+        text: 'welcome', // plaintext body
+        html: '<b>welcome</b>', // HTML body content
+      })
+      .then((success) => {
+        console.log(success)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+  }
+
+  public example2(): void {
+    this
+      .mailerService
+      .sendMail({
+        to: 'knals.developer@gmail.com', // List of receivers email address
+        from: 'user@outlook.com', // Senders email address
+        subject: 'Testing Nest Mailermodule with template ✔',
+        template: 'index', // The `.pug` or `.hbs` extension is appended automatically.
+        context: {  // Data to be sent to template engine.
+          code: 'cf1a3f828287',
+          username: 'john doe',
+        },
+      })
+      .then((success) => {
+        console.log(success)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+  }
+
+  public example3(): void {
+    this
+      .mailerService
+      .sendMail({
+        to: 'test@nestjs.com',
+        from: 'noreply@nestjs.com',
+        subject: 'Testing Nest Mailermodule with template ✔',
+        template: __dirname + '/index', // The `.pug` or `.hbs` extension is appended automatically.
+        context: {  // Data to be sent to template engine.
+          code: 'cf1a3f828287',
+          username: 'john doe',
+        },
+      })
+      .then((success) => {
+        console.log(success)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
   }
 }
