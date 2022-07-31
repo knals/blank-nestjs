1acacec2b3c0482cdf034366a1cb680d4022b5f5
knals <knals.developer@gmail.com>
2022-07-31 23:04:11 +0200

    hardcoded yaml fix

--- src/utils/configuration.ts
+++ src/utils/configuration.ts
@@ -1,8 +1,9 @@
 import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
-
-const YAML_CONFIG_FILENAME = `../../../src/enviroments/.config.yaml`;
+const YAML_CONFIG_FILENAME = `../config/enviroments/${
+  process.env.NODE_ENV ? process.env.NODE_ENV : ''
+}.config.yaml`;
 console.log(__dirname);
 
 export default () => {
