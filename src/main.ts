85bda3081eed4daaea7620bbf0d4ac59a0548104
knals <knals.developer@gmail.com>
2022-08-04 15:57:50 +0200

    nestjs migrated to v9

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT || 3000);
+  const config = new DocumentBuilder()
+    .setTitle('Swagger API example')
+    .setDescription('The example API description')
+    .setVersion('1.0')
+    .addTag('example')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 81);
 }
 bootstrap();
